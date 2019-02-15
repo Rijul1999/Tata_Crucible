18dc7b9c1994a13158a328bf00737f7807846c31
Rijul1999 <rijul.rakhi@gmail.com>
2019-02-15 19:26:32 +0530

    Update pothole_detect.py
    added starter code to make layer

--- pothole_detect.py
+++ pothole_detect.py
@@ -1,1 +1,19 @@
+"""
+This is the code file for detection of potholes on the roads of India. We use the gmaps.js library to implement our algorithm on the Google Maps API
+
+"""
+api_key = "3OC08f7c9ad7s9tutturu7v07sdv666"
+import gmaps
+#configure api
+gmaps.configure(api_key=api_key)
+#Define location 1 and 2
+Durango = (37.2753,-107.880067)
+SF = (37.7749,-122.419416)
+#Create the map
+fig = gmaps.figure()
+#create the layer
+layer = gmaps.directions.Directions(Durango, SF,mode='car')
+#Add the layer
+fig.add_layer(layer)
+fig
 
